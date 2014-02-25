aa5960dac0110fca1ca7e5af54f433f1d7043ed2
Iuri Andreazza <iuri.andreazza@gmail.com>
2014-02-25 14:18:30 -0300

    - Adicionando Stage 1 ao desafio

---
+++ desafio/stage1/puppet/modules/apache/manifests/conf.pp
@@ -0,0 +1,8 @@
+#Add a apache conf file
+define apache::conf() {
+  file { "/etc/apache2/${name}":
+    source  => "puppet:///modules/apache/${name}",
+    require => Package['apache2'],
+    notify  => Service['apache2'];
+  }
+}
